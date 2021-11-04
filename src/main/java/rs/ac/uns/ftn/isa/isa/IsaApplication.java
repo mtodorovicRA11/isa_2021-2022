4251eafa822e6c9e2b9caa27c70290820d52ea9d
Milan Todorovic <todorovic.ra11.2011@uns.ac.rs>
2021-11-04 22:12:36 +0100

    Initial commit with Spring Initializr

---
+++ src/main/java/rs/ac/uns/ftn/isa/isa/IsaApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.isa.isa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsaApplication.class, args);
+	}
+
+}
