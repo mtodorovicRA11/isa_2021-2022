fb08154fb4e48e2f72337ab13a15ebf6aebdaaaf
Boban <bobanpoznanovic1@gmail.com>
2021-11-21 19:39:20 +0100

    init zahtev za registrciju

---
+++ src/main/java/rs/ac/uns/ftn/isa/isa/repository/ZahtevZaRegistracijuRepository.java
@@ -0,0 +1,7 @@
+package rs.ac.uns.ftn.isa.isa.repository;
+
+import rs.ac.uns.ftn.isa.isa.model.ZahtevZaRegistraciju;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ZahtevZaRegistracijuRepository extends CrudRepository<ZahtevZaRegistraciju, Integer> {
+}
