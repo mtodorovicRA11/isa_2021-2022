42063f141df1dbd9b9f124d58fa08711e83c1715
alienunicorn <katarina.kaca.pantovic@gmail.com>
2021-12-07 10:39:39 +0100

    Added Jwt Filter and Util for checking and generating tokens

---
+++ src/main/java/rs/ac/uns/ftn/isa/isa/security/JwtFilter.java
@@ -0,0 +1,59 @@
+package rs.ac.uns.ftn.isa.isa.security;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final String AUTHORIZATION = "Authorization";
+    private final String BEARER = "Bearer ";
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            final Optional<String> jwtTokenOptional = extractToken(request);
+            if (jwtTokenOptional.isPresent()) {
+                final Authentication authentication = JwtUtils.getAuthentication(jwtTokenOptional.get());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            filterChain.doFilter(request, response);
+        } catch ( ExpiredJwtException e) {
+            returnUnauthorizedResponse(response, "Access token is expired!");
+        } catch ( JwtException e) {
+            returnUnauthorizedResponse(response, "Access token is invalid!");
+        }
+    }
+
+    private Optional<String> extractToken(HttpServletRequest request) {
+        final String bearerToken = request.getHeader(AUTHORIZATION);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER)) {
+            final String jwtToken = bearerToken.substring(BEARER.length());
+            return Optional.of(jwtToken);
+        }
+        return Optional.empty();
+    }
+
+    private void returnUnauthorizedResponse(HttpServletResponse httpServletResponse, String message) throws IOException {
+        httpServletResponse.addHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+
+        httpServletResponse.setContentType("application/json");
+        httpServletResponse.setCharacterEncoding("UTF-8");
+
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, message);
+    }
+}
---
+++ src/main/java/rs/ac/uns/ftn/isa/isa/security/JwtUtils.java
@@ -0,0 +1,36 @@
+package rs.ac.uns.ftn.isa.isa.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import org.springframework.stereotype.Component;
+import rs.ac.uns.ftn.isa.isa.model.User;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Collections;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    private static final int TOKEN_VALID_FOR_DAYS = 365;
+    private static final String SECRET_KEY = "ISA2021BOATCOTTAGE1234567890";
+    private static final String AUTHORITIES_KEY = "auth";
+
+    public static String generateJwtToken(User user) {
+        return Jwts.builder().setSubject(user.getId().toString()).claim(AUTHORITIES_KEY, user.getRole()).signWith(SignatureAlgorithm.HS512, SECRET_KEY).setExpiration(Date.from(ZonedDateTime.now(ZoneId.of("UTC")).plusDays(TOKEN_VALID_FOR_DAYS).toInstant())).compact();
+    }
+
+    public static Authentication getAuthentication(String token) {
+        final Claims claims = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+        final Long userId = Long.valueOf(claims.getSubject());
+        final String role = claims.get(AUTHORITIES_KEY).toString();
+
+        return new PreAuthenticatedAuthenticationToken(userId, null, Collections.singletonList(new SimpleGrantedAuthority(role)));
+    }
+
+}
